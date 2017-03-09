1233ad65ed820b45b940afcd3cc15302b2ca6188
Octavio Cedeno <ocedeno@Octavios-MacBook-Pro.local>
2017-03-09 09:05:38 -0500

    Docs: Added PhotoCollectionViewCell file
    This will allow me to adjust the Photo ImageView and Loading View objects in the Photo Collection View

---
+++ Virtual Tourist 2/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Virtual Tourist 2
+//
+//  Created by Octavio Cedeno on 3/9/17.
+//  Copyright © 2017 Cedeno Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell
+{
+        
+}
