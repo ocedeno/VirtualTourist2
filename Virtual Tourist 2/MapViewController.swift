adf8f88a668944d37878ca1ecd9b6e066ae52f58
Octavio Cedeno <ocedeno@Octavios-MacBook-Pro.local>
2017-03-09 08:27:42 -0500

    docs: Added MapViewController

---
+++ Virtual Tourist 2/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  Virtual Tourist 2
+//
+//  Created by Octavio Cedeno on 3/9/17.
+//  Copyright © 2017 Cedeno Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+}
