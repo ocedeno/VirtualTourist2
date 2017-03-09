2365d8bc3546917ac0142d63c35004998a7cb83c
Octavio Cedeno <ocedeno@Octavios-MacBook-Pro.local>
2017-03-09 11:15:13 -0500

    Docs: Created FlickrClient class
    Will use this class to create getRequest for Flickr API

---
+++ Virtual Tourist 2/FlickrClient.swift
@@ -0,0 +1,69 @@
+//
+//  FlickrClient.swift
+//  Virtual Tourist 2
+//
+//  Created by Octavio Cedeno on 3/9/17.
+//  Copyright © 2017 Cedeno Enterprises, Inc. All rights reserved.
+//
+
+import Foundation
+
+class FlickrClient
+{
+    static let sharedClient: FlickrClient = FlickrClient()
+    fileprivate var session = URLSession.shared
+    
+    struct flickrConstants
+    {
+        static let ApiScheme = "https"
+        static let ApiHost = "api.flickr.com"
+        static let APIPath = "/services/rest"
+        static let ApiMethod = "flickr.photos.search"
+        static let APIKey = "8f254826530527a1a83d8e8bdf2568a7"
+        static let SafeSearch = 1
+        static let extras = "url_m,date_taken"
+        static let format = "json"
+        static let nojsoncallback = 1
+        static let SearchBBOXHalfWidth:Float = 0.01
+        static let SearchBBOXHalfHeight:Float = 0.01
+        static let SearchLatRange:(Float, Float) = (-90.0, 90.0)
+        static let SearchLonRange:(Float, Float) = (-180.0, 180.0)
+    }
+    
+    struct UIConstants
+    {
+        static let MaxPhotoCount = 21
+        static let MaxPageCount = 15
+        static let MaxItemsPerPage = 16
+    }
+    
+    fileprivate func errorCheck(_ data: NSData?, response: URLResponse?, error: NSError?) -> NSError?
+    {
+        func sendError(_ error: String) -> NSError
+        {
+            print(error)
+            let userInfo = [NSLocalizedDescriptionKey: error]
+            return NSError(domain: "taskForGetMethod", code: -1, userInfo: userInfo)
+        }
+        
+        //was there an error
+        guard (error == nil) else
+        {
+            return sendError("There was an error with your request \(error)")
+        }
+        
+        //did we get a successful response from the API?
+        guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else
+        {
+            return sendError("There was an error with your request.  Status code is \((response as? HTTPURLResponse)?.statusCode)")
+        }
+        
+        //guard was there any data returned
+        guard let _ = data else
+        {
+            return sendError("Data was not found")
+        }
+        
+        return nil
+    }
+}
