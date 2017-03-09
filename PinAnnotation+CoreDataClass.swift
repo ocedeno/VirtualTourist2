ecf173a4d43e8971be197ed767021f77022b1bcd
Octavio Cedeno <ocedeno@Octavios-MacBook-Pro.local>
2017-03-09 13:52:43 -0500

    Docs: Created the init methods for the Core Data PinAnnotation Class.

--- PinAnnotation+CoreDataClass.swift
+++ PinAnnotation+CoreDataClass.swift
@@ -12,5 +12,31 @@ import CoreData
 
 public class PinAnnotation: NSManagedObject
 {
-
+    struct Keys {
+        static let latitude = "latitude"
+        static let longitude = "longitude"
+    }
+    
+    override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertInto: context)
+    }
+    
+    init(context: NSManagedObjectContext) {
+        guard let pinEntity = NSEntityDescription.entity(forEntityName: "PinAnnotation", in: context) else {
+            fatalError("Could not create Pin Entity Description!")
+        }
+        
+        super.init(entity: pinEntity, insertInto: context)
+    }
+    
+    init(dictionary: [String:AnyObject], context: NSManagedObjectContext) {
+        guard let pinEntity = NSEntityDescription.entity(forEntityName: "PinAnnotation", in: context) else {
+            fatalError("Could not create Pin Entity Description!")
+        }
+        
+        super.init(entity: pinEntity, insertInto: context)
+        
+        latitude = dictionary[Keys.latitude] as! Double
+        longitude = dictionary[Keys.longitude] as! Double
+    }
 }
