41f4c7dec64977f9f8ea90bb1254ffc070c45042
Octavio Cedeno <ocedeno@Octavios-MacBook-Pro.local>
2017-03-09 07:35:46 -0500

    Initial Commit

---
+++ Virtual Tourist 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist 2
+//
+//  Created by Octavio Cedeno on 3/9/17.
+//  Copyright © 2017 Cedeno Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
